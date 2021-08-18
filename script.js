00e77fa307d4b42d1fe3dcc737e91879d413a222
Teresa Banks <teresabanks1025@gmail.com>
2021-08-17 20:00:22 -0400

    validInput

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // Write your JavaScript code here!
 
+const validateInput = require("./scriptHelper");
+
 window.addEventListener("load", function() {
     let form = document.querySelector("form");
       form.addEventListener("submit", function(event) {
          let pilotNameInput = document.querySelector("input[name=pilotName]");
-         let coPilot = document.querySelector("input[name=coPilot]");
+         let coPilot = document.querySelector("input[name=copilotName]");
+         let fuelLevel= document.querySelector("input[name=fuelLevel]");
+         let cargoMass = document.querySelector("input[name=cargoMass]");
+        validateInput.formSubmission
+
+         //let test = validateInput;
+         //validateInput.validateInput(pilotNameInput.value);
+         /*
          if (pilotNameInput.value === "" || coPilot.value === "") {
             alert("All fields are required!");
          } else if (fuelLevel.value === "" || cargoMass.value === ""){
             alert("All fields are required!");
          }
+         */
             event.preventDefault();
          });
       
