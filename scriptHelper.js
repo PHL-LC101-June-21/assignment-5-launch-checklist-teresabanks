f25b4c7fa14959a9724a9f40dc0526b09a708d60
Teresa Banks <teresabanks1025@gmail.com>
2021-08-23 20:28:38 -0400

    added list requirements SIP

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,59 +2,74 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-    if (testInput === ""){
+    if (testInput === "") {
         return "Empty";
-    } else if (isNaN(testInput)){
+    } else if (isNaN(testInput)) {
         return "Not a Number"
     } else {
         return "Is a Number"
     };
-    
+
 };
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   let validPilotInput = validateInput(pilot);
-   let validCopilotInput = validateInput(copilot);
-   let validFuelLevel = validateInput(fuelLevel);
-   let validCargoLevel = validateInput(cargoLevel);
-   
-   if(validPilotInput === "Empty" || validCopilotInput === "Empty" ||
-      validFuelLevel === "Empty" || validCargoLevel === "Empty"){
-    return alert("All fields are required!");
-   }
-   else if (validFuelLevel === "Not a Number" || validCargoLevel === "Not a Number"){
-       return alert("Enter Valid Information for Each Field");
-   }
- 
-list.style.visibility = "visible";
-document.getElementById("pilotStatus").innerHTML = `Pilot ${validPilotInput} is ready for Launch`;
-document.getElementById("copilotStatus").innerHTML = `Copilot ${validCopilotInput} is ready for Launch`;
- }
- }
+    let validPilotInput = validateInput(pilot);
+    let validCopilotInput = validateInput(copilot);
+    let validFuelLevel = validateInput(fuelLevel);
+    let validCargoLevel = validateInput(cargoLevel);
+
+    if (validPilotInput === "Empty" || validCopilotInput === "Empty" ||
+        validFuelLevel === "Empty" || validCargoLevel === "Empty") {
+        return alert("All fields are required!");
+    }
+    else if (validFuelLevel === "Not a Number" || validCargoLevel === "Not a Number") {
+        return alert("Enter Valid Information for Each Field");
+    }
+
+    list.style.visibility = "visible";
+    document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for Launch`;
+    document.getElementById("copilotStatus").innerHTML = `Copilot ${copilot} is ready for Launch`;
+
+    if (validFuelLevel < 10000){
+        document.getElementById("fuelStatus").innerHTML = "Fuel too low for launch";
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+    } else if (validCargoLevel > 10000){
+        document.getElementById("cargoStatus").innerHTML = "Cargo level too high for launch";
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+    }
+
+    if(validCargoLevel > 10000 && validFuelLevel < 10000){
+        document.getElementById("launchStatus").style.color = "green";
+        document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch";
+    }
+}
 
 
-   
+
+
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -66,5 +81,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
