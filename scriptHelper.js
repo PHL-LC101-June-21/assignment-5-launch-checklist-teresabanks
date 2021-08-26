c3767c39345d306f795755a5b4e62b597d9de7dc
Teresa Banks <teresabanks1025@gmail.com>
2021-08-26 18:53:37 -0400

    latest updates

--- scriptHelper.js
+++ scriptHelper.js
@@ -40,26 +40,27 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass) {
     else if (validFuelLevel === "Not a Number" || validCargoLevel === "Not a Number") {
         return alert("Enter Valid Information for Each Field");
     } 
-
+    list.style.visibility = "hidden"
     list.style.visibility = "visible";
+    
     document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`;
     document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`;
 
     if (fuelLevel < 10000){
         document.getElementById("fuelStatus").innerHTML = "Fuel level too low for launch";
-        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for launch";
         document.getElementById("launchStatus").style.color = "rbg(199,37,78";
         
     }
     
     if (cargoMass > 10000){
-        document.getElementById("cargoStatus").innerHTML = "Cargo mass too high for launch";
+        document.getElementById("cargoStatus").innerHTML = "Cargo mass too heavy for launch";
         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
         document.getElementById("launchStatus").style.color = "rbg(199,37,78)";
     } 
     
     if (cargoMass < 10000 && fuelLevel > 10000){
-        document.getElementById("launchStatus").style.color = "green";
+        document.getElementById("launchStatus").style.color = "rgb(65, 159, 106)";
         document.getElementById("launchStatus").innerHTML = "Shuttle is Ready for Launch";
         document.getElementById("cargoStatus").innerHTML = "Cargo mass low enough for launch"
         document.getElementById("fuelStatus").innerHTML = "Fuel level high enough for launch"
